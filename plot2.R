69656b6f800762425bb5731efe1f366c00d517bc
Charles Szpara <cszpara@me.com>
2017-11-22 18:49:22 -0500

    Course Project 1 Submittal

---
+++ plot2.R
@@ -0,0 +1,32 @@
+#Check for data availability. If not, download file.
+filename <- "exdata_data_household_power_consumption.zip"
+if(!file.exists(filename)){
+  Location1 <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(Location1, filename, mode = "wb", cacheOK = FALSE)
+  rm(Location1)
+}
+if(!file.exists("UCI HAR Dataset")) {
+  unzip(filename)
+}
+rm(filename)
+
+#Import and Subset Data in R
+Dataset <- read.table("./household_power_consumption.txt",sep = ";",header = T)
+LogicalDate <- Dataset$Date == "1/2/2007" |  Dataset$Date == "2/2/2007"
+Dataset <- subset(Dataset, LogicalDate)
+row.names(Dataset) <- 1:nrow(Dataset)
+rm(LogicalDate)
+
+#Format Date and Time columns
+Dataset$Date <- strptime(paste(Dataset$Date,Dataset$Time,sep = ""),"%d/%m/%Y %H:%M:%S")
+Dataset <- subset(Dataset,select = -c(Time))
+
+#Format Other Columns as Numeric
+Dataset[ ,2:ncol(Dataset)] <- apply(Dataset[ ,2:ncol(Dataset)], 2, function(x) as.numeric(x))
+
+#Plot 2
+png("plot2.png", width = 480, height = 480, units = 'px')
+Y.Title <- "Global Active Power (kilowatts)"
+with(Dataset,plot(y = Global_active_power, x = Date, type = "l",
+                  ylab = Y.Title, xlab = "", cex.lab = 1))
+dev.off()
